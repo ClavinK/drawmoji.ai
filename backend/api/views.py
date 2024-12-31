ca890389ee3151d8fa631d0dccff8a209c282ded
omega <omega@gmail.com>
2024-12-30 21:57:41 -0800

    Updated URLs

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Example Code with HttpResponse class
+from django.http import HttpResponse
+def say_hello(request):
+    return HttpResponse('Hello World')
